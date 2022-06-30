3a8ac2f592bee23961c904f0d746d98d48620aec
PRAMODSHENDAGE <102169349+PRAMODSHENDAGE@users.noreply.github.com>
2022-06-30 23:16:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day27_File_IO_PP;
+
+Console.WriteLine("Hello, World!");
+
+FileIO.FileExists();
+FileIO.ReadAllLines();
+FileIO.ReadAllText();
+FileIO.FileCopy();
+FileIO.DeleteFile();
+FileIO.ReadFromStreamReader();
+FileIO.WriteUsingStreamWriter();
+
+
+
+//MultipleAddressBook obj = new MultipleAddressBook();
+//obj.DictionaryContacts();
+
+//AddMultipleContact addressBook = new AddMultipleContact();
+
+//addressBook.AddMultipleContact1();
+
+//addressBook.AddNewContact();
+//addressBook.AddNewContact();
+
+//AddMultipleContact.PrintAddressBook();
+//Console.WriteLine("Enter the FirstName to Edit");
+//string firstName = Console.ReadLine();
+//addressBook.EditContact(firstName);
+//AddMultipleContact.PrintAddressBook();
+
+//AddMultipleContact.PrintAddressBook();
+//Console.WriteLine("Enter the FirstName to Delete");
+//string firstName1 = Console.ReadLine();
+//addressBook.DeleteContact(firstName1);
+//AddMultipleContact.PrintAddressBook();
